25403a3f124979b8f110ecfba0ec6d594d0e7db6
Karen V <karenvo@usc.edu>
2020-01-18 11:27:55 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import MovieReview from './MovieReview'
 
-const MovieReviews = ({ reviews }) => (
-    <ul className="review-list">
-        {/* {console.log(reviews)} */}
-        {reviews.map(review => (<MovieReview title={review.display_title} summary={review.summary_short}/>))}
-        {/* {movies.map(movie => (<Movie  movieInfo={movie}/>))} */}
-    </ul>
-)
 
-export default MovieReviews; 
+const MovieReviews = (props) => {
+  return(
+    <div className='review-list'>
+     {props.reviews.map(review => 
+        <li key={review.display_title} className='review'>
+            <h1>{review.headline}</h1>
+        </li>
+      )}
+    </div>
+  )}
+
+
+export default MovieReviews 
