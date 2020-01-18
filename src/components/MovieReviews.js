6b50801b812322f1348e731ba1840e1c74a50033
Karen V <karenvo@usc.edu>
2020-01-18 11:23:56 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,11 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReview = ({ title, summary }) => (
+    <li className='review'>
+        <h1>{title}</h1>
+        <p>{summary}</p>
+    </li>
+)
+
+export default MovieReview 
